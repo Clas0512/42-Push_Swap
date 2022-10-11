618a40cbe5ef6c958f40e049bb99dd88e6282bfd
Clas0512 <mrb.bn@hotmail.com>
2022-10-12 00:56:52 +0300

    first

---
+++ push_swap_falan/push_swap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anargul <anargul@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/11 21:37:22 by anargul           #+#    #+#             */
+/*   Updated: 2022/10/12 00:46:21 by anargul          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdio.h>
+# include "./libft/libft.h"
+
+typedef struct s_data
+{
+	int		size;
+}	t_data;
+
+#endif
